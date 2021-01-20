23ce1827dce4f94a11a178777ad74b694fd98033
Jack Bennett <73858124+j-ack-bennett@users.noreply.github.com>
2021-01-20 16:40:53 +1300

    started blog

---
+++ seeds/3-posts.js
@@ -0,0 +1,30 @@
+exports.seed = function (knex, Promise) {
+  return knex('posts').insert([
+    { id: 77701, user_id: 99901, title: '', content: '' },
+    { id: 77702, user_id: 99902, title: '', content: '' },
+    { id: 77703, user_id: 99903, title: '', content: '' },
+    { id: 77704, user_id: 99904, title: '', content: '' },
+    { id: 77705, user_id: 99905, title: '', content: '' },
+    { id: 77706, user_id: 99906, title: '', content: '' },
+    { id: 77707, user_id: 99907, title: '', content: '' },
+    { id: 77708, user_id: 99908, title: '', content: '' },
+    { id: 77709, user_id: 99909, title: '', content: '' },
+    { id: 77710, user_id: 99910, title: '', content: '' },
+    { id: 77711, user_id: 99911, title: '', content: '' },
+    { id: 77712, user_id: 99912, title: '', content: '' },
+    { id: 77713, user_id: 99913, title: '', content: '' },
+    { id: 77714, user_id: 99914, title: '', content: '' },
+    { id: 77715, user_id: 99915, title: '', content: '' },
+    { id: 77716, user_id: 99916, title: '', content: '' },
+    { id: 77717, user_id: 99917, title: '', content: '' },
+    { id: 77718, user_id: 99918, title: '', content: '' },
+    { id: 77719, user_id: 99919, title: '', content: '' },
+    { id: 77720, user_id: 99920, title: '', content: '' },
+    { id: 77721, user_id: 99921, title: '', content: '' },
+    { id: 77722, user_id: 99922, title: '', content: '' },
+    { id: 77723, user_id: 99923, title: '', content: '' },
+    { id: 77724, user_id: 99924, title: '', content: '' },
+    { id: 77725, user_id: 99925, title: '', content: '' },
+    { id: 77726, user_id: 99926, title: '', content: '' }
+  ])
+}
