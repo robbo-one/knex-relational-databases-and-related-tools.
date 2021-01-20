406f8ebece3cf0965f0396bf8936808cb1b4a7ec
Isaac Sedcole <44042755+Isaac-Sedcole@users.noreply.github.com>
2021-01-20 15:51:48 +1300

    completed posts and blogs stuff

--- db.js
+++ db.js
@@ -6,7 +6,11 @@ module.exports = {
   getUser: getUser,
   getUsers: getUsers,
   createUser: createUser,
-  createProfile: createProfile
+  createProfile: createProfile,
+  getUsersData: getUsersData,
+  getPosts: getPosts,
+  makePost: makePost,
+  getAllPostsPerson: getAllPostsPerson
 }
 
 function getUsers (db = connection) {
@@ -29,4 +33,27 @@ function createUser (name, email, db = connection){
 function createProfile (user_id,url, picture, db = connection){
   return db ('profiles')
   .insert ({user_id:user_id, url:url, picture:picture})
+}
+function getUsersData (id, db = connection){
+  return db('users')
+  .where('users.id', id)
+  .first()
+  .join('profiles','users.id','profiles.user_id')
+  .join('posts', 'users.id', 'posts.user_id')
+  .select('*', "users.id AS id")
+}
+function getPosts (db = connection){
+  return db ('posts')
+  .select()
+}
+
+function makePost (id, title, content, db = connection){
+  return db ('posts')
+  .insert({user_id:id , title:title , content:content})
+}
+
+function getAllPostsPerson(id, db = connection) {
+  return db('posts')
+  .where('user_id', id)  
+  .select('*')
 }
--- users.js
+++ users.js
@@ -16,15 +16,56 @@ router.get('/', (req, res) => {
 
 router.get('/profile/:id', (req, res) => {
   const id = req.params.id
-  db.getUser(id)
+  //else {getAllPosts}
+
+  let person = {
+    id: id,
+    name: "",
+    email: "",
+    url: "",
+    picture: "",
+     posts : { title: "",
+              content : ""}    
+  }
+  db.getUsersData(id)
     .then(user => {
-      // console.log(user)
-      return res.render('profilePage',  user )
+      person.id = user.id 
+      person.name = user.name
+      person.email = user.email
+      person.url = user.url
+      person.picture = user.picture
+      db.getAllPostsPerson(id) 
+      .then(post => {
+        person.posts = post
+        return res.render('profilePage',  person)
+      })
+      //  console.log(user)
+
+      // return res.render('profilePage',  user)
+      // .catch(err => {
+      //   res.status(500).send('DATABASE ERROR: ' + err.message)
     })
     .catch(err => {
       res.status(500).send('DATABASE ERROR: ' + err.message)
     })
 })
+router.post('/profile/:id', (req, res) => {
+  const id = req.params.id
+  let newPost = {
+    title: req.body.title,
+    content: req.body.content,
+  }
+  db.makePost(id, newPost.title , newPost.content)
+    .then(() => {
+      res.redirect('/profile/' + id )
+    })
+    .catch(err => {
+      res.status(500).send('DATABASE ERROR: ' + err.message)
+    })
+})
+
+
+
 
 router.post("/addUser", (req, res) => {
   let newUser = {
@@ -41,6 +82,16 @@ router.post("/addUser", (req, res) => {
         res.redirect("/")
       })
       })
+})
+
+router.get('/posts', (req,res) => {
+  db.getPosts()
+  .then(posts => {
+    return res.render('blogPage', { posts: posts })
+  })
+  .catch(err => {
+    res.status(500).send('DATABASE ERROR: ' + err.message)
+  })
 })
 
 router.get("/addUser", (req, res) => {
